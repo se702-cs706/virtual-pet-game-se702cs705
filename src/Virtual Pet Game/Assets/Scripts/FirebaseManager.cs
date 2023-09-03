a7bf9b90dcc77a7a013a6edcf2b9fde9626afa76
jzho987 <jzho987@aucklanduni.ac.nz>
2023-09-03 21:26:49 +1200

    Added firestore packages

---
+++ src/Virtual Pet Game/Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    // initialize Firebase
+    private void Awake()
+    {
+    }
+
+
+}
