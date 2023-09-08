19dda0c0bc4f694fefd0f26581bd71114eb2c2f4
Plak-xD <akup732@aucklanduni.ac.nz>
2023-09-08 17:58:59 +1200

    Picks random location, moves to it and sniffs, pick a new random location

--- src/Virtual Pet Game/Assets/Resources/DogBehaviour.cs
+++ src/Virtual Pet Game/Assets/Resources/DogBehaviour.cs
@@ -9,12 +9,14 @@ public class DogBehaviour : MonoBehaviour
     private double targetPosY;
     private double targetPosZ;
     private int moveSpeed = 2;
+    private float[] target = {0f, 0f, 0f};
+    private Vector3 position;
+    private float targetTime;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
-        pickTarget();
 
     }
 
@@ -22,30 +24,106 @@ public class DogBehaviour : MonoBehaviour
     void Update()
     {
 
-        move();
+        position = GameObject.Find("Capsule Dog").transform.position;
+
+        if (Math.Abs(target[0] - position[0]) > 0.2 || Math.Abs(target[2] - position[2]) > 0.2)
+        {
+
+            move();
+
+        } else
+        {
+
+            if (targetTime < Time.time)
+            {
+
+                Debug.Log("SNIFF SNIFF");
+                targetTime = Time.time + 3.5f;
+
+            }
+
+            if (targetTime - Time.time < 0.5f)
+            {
+
+                pickTarget();
+
+            }
+
+            if (targetTime - Time.time > 0.5)
+            {
+
+                transform.eulerAngles = new Vector3(customNormalDist(320f, 1f, 1f, 2.6f, -15f, 0f, targetTime - Time.time) +
+                    customNormalDist(320f, 1f, 1f, 1.8f, -15f, 0f, targetTime - Time.time), 0f, 0f);
+
+            }
+
+        }
 
     }
 
     void move()
     {
 
-        if (Math.Round(Time.time) % 2 <= 0)
-        {
-
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-
-        }
+        transform.Translate(new Vector3(moveFunc(target[0], position[0]), 0, moveFunc(target[2], position[2])) * moveSpeed * Time.deltaTime);
 
     }
 
     void pickTarget()
     {
 
-        var position = GameObject.Find("Capsule Dog").transform.position;
+        position = GameObject.Find("Capsule Dog").transform.position;
         Debug.Log("Current Position: " + position);
-        var x = UnityEngine.Random.Range(-5f,5f);
-        Debug.Log(x);
+        var x = position[0] + UnityEngine.Random.Range(-5f,5f);
+        var z = position[2] + UnityEngine.Random.Range(-5f,5f);
+
+        if (x > 15)
+        {
+            x = 15;
+        }
+
+        if (x < -15)
+        {
+            x = -15;
+        }
+
+        if (z > 10)
+        {
+            x = 10;
+        }
+
+        if (z < -20)
+        {
+            z = -20;
+        }
+
+        float[] randomPosition = {x, position[1], z};
+        Debug.Log("Random Position: " + randomPosition[0] + ", " + randomPosition[1] + ", " + randomPosition[2]);
+        target = randomPosition;
+
         transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+    }
+
+    float moveFunc(float target, float position)
+    {
+
+        float output = (float)(Math.Pow(Math.Abs(target - position) / 7.0f, 1.0 / 7.0)) * Math.Abs(target - position) / (target - position);
+        if (output == Double.NaN || Double.IsNaN(output))
+        {
+
+            output = 0f;
+
+        }
+
+        //Debug.Log("Target: " + target + " Position: " + position + " Output: " + output);
+        return output;
+
+    }
+
+    float customNormalDist(float A, float B, float C, float D, float E, float F, float x)
+    {
+
+        return A / ((1f + (float) Math.Pow(Math.E, B * E * (x - D))) * (1f + (float)Math.Pow(Math.E, -1f * C * E * (x - D)))) + F;
 
     }
 }
