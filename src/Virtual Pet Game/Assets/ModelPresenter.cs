37ce0b1ddcb594e8058e1a7fe6e248084a5f254f
jzho987 <79814775+jzho987@users.noreply.github.com>
2023-09-15 14:10:52 +1200

    feat: updated the animation to look more realistic

--- src/Virtual Pet Game/Assets/ModelPresenter.cs
+++ src/Virtual Pet Game/Assets/ModelPresenter.cs
@@ -12,7 +12,6 @@ public class ModelPresenter : MonoBehaviour
     [SerializeField] private ModelBalancer model;
 
     [SerializeField] private float maxSpeed = 1.3f;
-    [SerializeField] private float rotationCullDist = 0.3f;
 
     [SerializeField] private float multiplier;
     [SerializeField] private float predictionRate;
@@ -26,20 +25,16 @@ public class ModelPresenter : MonoBehaviour
         var direction = new Vector3(lookPos.x - transform.position.x, transform.forward.y, lookPos.z - transform.position.z);
         
         var speed = Math.Min(multiplier * direction.magnitude, maxSpeed);
-        if (direction.magnitude < rotationCullDist)
-        {
-            direction = transform.forward;
-            speed = 0;
-        }
         
         // set all values
         model.setLook(direction);
         view.setSpeed(speed);
-        var angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
         
-        if (angle > 30 || angle < -30)
+        // set the angle for drift
+        var angle = Vector3.SignedAngle(transform.forward, direction, Vector3.up);
+        if (angle > 20 || angle < -20)
         {
-            view.setDrift(angle / 30);
+            view.setDrift(angle / 199);
         }
         else
         {
