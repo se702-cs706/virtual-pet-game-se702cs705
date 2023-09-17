45fe6873ad8fd46199b834418bf4a9ad2eadc36e
jzho987 <79814775+jzho987@users.noreply.github.com>
2023-09-17 16:08:43 +1200

    feat: added the state interface and the state manager

--- src/Virtual Pet Game/Assets/DogManager.cs
+++ src/Virtual Pet Game/Assets/DogManager.cs
@@ -1,10 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/**
- * Dog model that holds the states
- */
+/// <summary>
+/// State pattern manager for the dog
+/// </summary>
 public class DogManager : MonoBehaviour
 {
+    private IState _currentState;
+    [Header("Params")] 
+    [SerializeField] private float Energy;
+    [SerializeField] private float Excitement;
+
+    private void Update()
+    {
+        var newState = _currentState.onStateUpdate();
+        if (newState != null)
+        {
+            _currentState.onStateExit();
+            _currentState = newState;
+            _currentState.onStateEnter();
+        }
+    }
 }
