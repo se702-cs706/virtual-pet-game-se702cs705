494b455f055f1c76629467e4e2832a4d8f7be8c8
jzho987 <79814775+jzho987@users.noreply.github.com>
2023-09-17 16:15:31 +1200

    feat: added params

--- src/Virtual Pet Game/Assets/DogManager.cs
+++ src/Virtual Pet Game/Assets/DogManager.cs
@@ -9,10 +9,13 @@ using UnityEngine;
 public class DogManager : MonoBehaviour
 {
     private IState _currentState;
+    private float time;
     [Header("Params")] 
-    [SerializeField] private float Energy;
-    [SerializeField] private float Excitement;
-
+    [SerializeField] private float Energy = 10;
+    [SerializeField] private float Excitement = 10;
+    [SerializeField] private float EnergyDropRatePS = 0.01f;
+    [SerializeField] private float ExcitementDropRatePS = 1f;
+    
     private void Update()
     {
         var newState = _currentState.onStateUpdate();
@@ -22,5 +25,23 @@ public class DogManager : MonoBehaviour
             _currentState = newState;
             _currentState.onStateEnter();
         }
+
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            time += 1;
+            Energy = Math.Max(0,Energy - EnergyDropRatePS);
+            Excitement = Math.Max(0,Excitement - ExcitementDropRatePS);
+        }
+    }
+
+    public float getEnergy()
+    {
+        return Energy;
+    }
+    
+    public float getExcitement()
+    {
+        return Excitement;
     }
 }
