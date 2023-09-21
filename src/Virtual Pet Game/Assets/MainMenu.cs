191646d5313f2b703f23dc3ff72e4731e286cf0d
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-09-21 22:14:31 +1200

    options_menu_readded

---
+++ src/Virtual Pet Game/Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
