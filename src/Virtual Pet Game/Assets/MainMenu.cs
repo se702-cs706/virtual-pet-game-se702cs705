cb445cf9d63c0dbd8977bdffd62204c6d17c823a
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-10-02 19:24:04 +1300

    error fixed

--- src/Virtual Pet Game/Assets/MainMenu.cs
+++ src/Virtual Pet Game/Assets/MainMenu.cs
@@ -8,13 +8,21 @@ public class MainMenu : MonoBehaviour
 
     public int currentMode = 1;  //mode 1: Practice, mode 2: Study
     public GameObject selectModel;
+    public GameObject MainSettings;
+    public GameObject GamePlay;
 
     public void PlayGame()
     {
         //SceneManager.LoadSceneAsync(1);
+
         if (currentMode == 2)
         {  
             selectModel.SetActive(true);
+        }
+        else
+        {
+            GamePlay.SetActive(true);
+            MainSettings.SetActive(false);
         }
     }
 
