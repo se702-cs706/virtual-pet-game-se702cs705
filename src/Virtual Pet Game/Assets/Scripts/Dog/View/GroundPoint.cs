ef8c025b6214b5989c6a730147a501ec916fb03e
J.Z. Zhou <79814775+jzho987@users.noreply.github.com>
2023-10-05 16:29:02 +1300

    Fix sideways pet (#56)
    Co-authored-by: AndyF <afon072@aucklanduni.ac.nz>
    Co-authored-by: AndyF <79813932+afon072@users.noreply.github.com>
    Co-authored-by: Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
    Co-authored-by: Lang Cheng (lche262) <79813230+lche262@users.noreply.github.com>
    Co-authored-by: AoMedium <anoddmedium@gmail.com>
    Co-authored-by: Plak-xD <79771780+Plak-xD@users.noreply.github.com>
    Co-authored-by: AoMedium <75078332+AoMedium@users.noreply.github.com>
    Co-authored-by: Plak-xD <akup732@aucklanduni.ac.nz>

--- src/Virtual Pet Game/Assets/Scripts/Dog/View/GroundPoint.cs
+++ src/Virtual Pet Game/Assets/Scripts/Dog/View/GroundPoint.cs
@@ -8,10 +8,12 @@ public class GroundPoint : MonoBehaviour
     [SerializeField] LayerMask groundLayer;
     [SerializeField] ModelBalancer _modelBalancer;
     public Vector3 _groundPoint { get; private set; } = Vector3.negativeInfinity;
+    public Vector3 _normal { get; private set; } = Vector3.up;
 
     void FixedUpdate()
     {
-        FindGroundPoint(Vector3.up);
+        FindGroundPoint(transform.up);
+        Debug.DrawLine(transform.position,transform.position + _normal, Color.blue);
     }
     
     void FindGroundPoint(Vector3 groundNormal)
@@ -19,11 +21,13 @@ public class GroundPoint : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -groundNormal, out hit, raycastDistance, groundLayer))
         {
-            _groundPoint = hit.point;   
+            _groundPoint = hit.point;
+            _normal = hit.normal;
         }
         else
         {
             _groundPoint = Vector3.negativeInfinity;
+            _normal = Vector3.up;
         }
     }
 }
